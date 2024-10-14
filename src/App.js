af7218bc35b0e09339ff2d020ce5dc646dfd849a
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-10-14 18:27:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -64,6 +64,11 @@ function Square({value,onSquareClick}) {
 }
 
 export default function Game (){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+
   return(
     <>
       <div className="game">
